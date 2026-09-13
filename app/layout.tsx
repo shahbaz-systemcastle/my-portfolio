3908084281b0a80f4df94975711be311071ec97b
Shahbaz-Systemcastle <shahbaz.hussain@systemcastle.com>
2026-09-14 01:15:43 +0500

    Update portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Shahbaz Hussain | Web & Mobile Developer",
+  description:
+    "Portfolio of Shahbaz Hussain, Web & Mobile Developer specializing in React Native, web development, API integrations, GoHighLevel, and modern frontend solutions.",
 };
 
 export default function RootLayout({
@@ -18,9 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
-      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
